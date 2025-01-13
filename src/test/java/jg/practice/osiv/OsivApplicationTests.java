93301c6b7e2405c05a1f191cd4b3ee1c51af7c07
ajroot5685 <anjeong8577@naver.com>
2025-01-13 15:10:51 +0900

    initial commit

---
+++ src/test/java/jg/practice/osiv/OsivApplicationTests.java
@@ -0,0 +1,13 @@
+package jg.practice.osiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
